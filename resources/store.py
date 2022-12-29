8b593afea93f650f8f26c64e91c089eb38abe506
mlysikov <maxim.lysikov@gmail.com>
2022-12-29 09:44:59 -0800

    First commit!

---
+++ resources/store.py
@@ -0,0 +1,41 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from models import StoreModel
+from schemas import StoreSchema
+from sqlalchemy.exc import SQLAlchemyError
+from db import db
+
+blp = Blueprint("stores", __name__, description="Operation on stores")
+
+@blp.route("/store/<int:store_id>")
+class Store(MethodView):
+    @blp.response(200, StoreSchema)
+    def get(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        return store
+
+    def delete(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}
+
+@blp.route("/store")
+class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
+    def get(self):
+        return StoreModel.query.all()
+
+    @blp.arguments(StoreSchema)
+    @blp.response(200, StoreSchema)
+    def post(self, store_data):
+        store = StoreModel(**store_data)
+
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occured while inserting the store.")
+        return store
